b2ae7c496e3810b844672dee70c43cf3625d5d7e
TheUltronX <96869020+TheUltronX@users.noreply.github.com>
2021-12-30 22:11:26 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,28 @@
 
+echo ">> FETCHING UPSTREAM..."
+git clone https://github.com/TheUltronX/MusicPlayer /MusicPlayer
+echo ">> INSTALLING REQUIREMENTS..."
+cd /MusicPlayer
+pip3 install -U -r requirements.txt
+echo ">> STARTING ULTRONXMUSIC PLAYER USERBOT..."
+clear
+echo "
+#     #                        
+##   ## #    #  ####  #  ####  
+# # # # #    # #      # #    # 
+#  #  # #    #  ####  # #      
+#     # #    #      # # #      
+#     # #    # #    # # #    # 
+#     #  ####   ####  #  ####  
+                               
+######                                    
+#     # #        ##   #   # ###### #####  
+#     # #       #  #   # #  #      #    # 
+######  #      #    #   #   #####  #    # 
+#       #      ######   #   #      #####  
+#       #      #    #   #   #      #   #  
+#       ###### #    #   #   ###### #    # 
+                                          
+MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
+"
+python3 main.py
